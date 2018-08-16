bed49cb24a4ee8966f0aaa9a93a940dc3df51e73
Riley <RP2@users.noreply.github.com>
2018-08-16 11:51:09 -0700

    col style

--- wayfarer/src/components/Landing/About.js
+++ wayfarer/src/components/Landing/About.js
@@ -4,19 +4,21 @@ class About extends Component {
     render() {
         return (
             <div className="About">
+                <div className="container-fluid">
                 <h2>About Wayfarer</h2>
-                <div>
-                    <div className="AboutTopic">
-                        <h3>Mission</h3>
-                        <p>Our goal is to help people discover new things about places they are visiting.</p>
-                    </div>
-                    <div className="AboutTopic">
-                        <h3>History</h3>
-                        <p>We have been in buisness since 2018.</p>
-                    </div>
-                    <div className="AboutTopic">
-                        <h3>Future</h3>
-                        <p>We hope to provide our users with an easy way to find interesting facts about cities.</p>
+                    <div className="row">
+                        <div className="col-sm">
+                            <h4>Mission</h4>
+                            <p>Our goal is to help people discover new things about places they are visiting.</p>
+                        </div>
+                        <div className="col-sm">
+                            <h4>History</h4>
+                            <p>We have been in buisness since 2018.</p>
+                        </div>
+                        <div className="col-sm">
+                            <h4>Future</h4>
+                            <p>We hope to provide our users with an easy way to find interesting facts about cities.</p>
+                        </div>
                     </div>
                 </div>
             </div>
