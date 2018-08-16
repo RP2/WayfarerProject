6e60deba634fe3086c3e37f4218fde23fc326fce
Riley <RP2@users.noreply.github.com>
2018-08-16 11:33:57 -0700

    added .gitignore

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -17,10 +18,17 @@ const db = require("./models");
 const controllers = require("./controllers");
 
 // ########################### ROUTES ###############################
-
+// define a root route: localhost:3000/
 app.get("/api", (req, res) => {
   res.send();
 });
+//login or create account/post routes
+app.post('/api/user/login', controllers.user.login);
+app.post("/api/user/signup", controllers.user.signup);
+app.post("/api/user/:user_id/post", controllers.user.post.create);
+
+app.get("/api/user", controllers.user);
+app.get("/api/user/:id/post", controllers.user.post.create);
 
 // listen on the port that Heroku prescribes (process.env.PORT) OR port 3000
 app.listen(process.env.PORT || 3000, () => {
