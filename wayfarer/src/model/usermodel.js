2340fe7d8a762d85a8412ba81d2eb8c6786c3fa7
Riley <RP2@users.noreply.github.com>
2018-08-16 14:21:47 -0700

    user models

--- wayfarer/src/model/usermodel.js
+++ wayfarer/src/model/usermodel.js
@@ -1,1 +1,17 @@
-import Axios from 'axios';
+import axios from 'axios'
+
+class loginModel {
+  static login(){
+    let request = axios.post(`http://localhost:4000/api/user/login`)
+    return request
+  }
+}
+
+class signupModel {
+  static signup(){
+    let request = axios.post(`http://localhost:4000/api/user/signup`)
+    return request
+  }
+}
+
+export default {loginModel, signupModel}
