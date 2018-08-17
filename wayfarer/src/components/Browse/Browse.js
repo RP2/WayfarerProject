a3713f8a087d9c508d3f46989032d0585ef36a2c
bjimison <brady.t.jimison@gmail.com>
2018-08-17 11:04:51 -0700

    styling and html for browse page

--- wayfarer/src/components/Browse/Browse.js
+++ wayfarer/src/components/Browse/Browse.js
@@ -1,15 +1,37 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Browse extends Component {
-    render() {
-        return (
-            <div className="Browse">
-            <h2>Browse</h2>
-            <p><Link to="/createpost">CreatePost</Link></p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="Browse">
+        <div className="cities-column">
+          <h2>Cities</h2>
+          <ul id="cities">
+            <li>
+              <img src="#" alt="#" />
+              <h5>Paris</h5>
+            </li>
+            <li>
+              <img src="#" alt="#" />
+              <h5>Tokyo</h5>
+            </li>
+            <li>
+              <img src="#" alt="#" />
+              <h5>London</h5>
+            </li>
+            <li>
+              <img src="#" alt="#" />
+              <h5>San Francisco</h5>
+            </li>
+          </ul>
+        </div>
+        <p id="createPost">
+          <Link to="/createpost">CreatePost</Link>
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Browse;
