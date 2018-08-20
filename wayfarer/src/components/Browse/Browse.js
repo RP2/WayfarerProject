12ca408dd2892333023f9e26c3a1c4bf5bd3baca
Riley <RP2@users.noreply.github.com>
2018-08-20 11:26:20 -0700

    merge

--- wayfarer/src/components/Browse/Browse.js
+++ wayfarer/src/components/Browse/Browse.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Model from "../../model/listPosts";
 
+
 class Browse extends Component {
+  componentDidMount() {
+    console.log("component did mount");
+    let username;
+    if (localStorage.getItem("username") === null) {
+      return this.props.history.push("/login");
+    } else {
+      username = localStorage.getItem("username");
+    }
+  }
+
 
     state = {
         posts: [],
